c54001ead8fc15b863ad31b8d3bd36f7a7f36b94
JZhou717 <32233263+JZhou717@users.noreply.github.com>
2019-07-01 14:53:01 -0400

    First upload. Board initialization complete
    Next steps: flesh out the runner class to ask for new game difficulty, and to read inputs. Must also add the methods for interaction to the board and tiles

---
+++ Minesweeper.java
@@ -0,0 +1,5 @@
+class Minesweeper {
+  public static void main(String[] args) {
+    Board b = new Board(5,3);
+  }
+}
---
+++ Tile.java
@@ -0,0 +1,25 @@
+/**
+* Represents a single tile in 
+**/
+
+class Tile {
+
+  protected int row_pos;
+  protected int col_pos;
+
+  protected boolean mine = false;
+  protected int adj_mines = 0;
+
+  protected boolean clicked = false;
+  protected boolean flag = false;
+
+  public Tile(int r, int c) {
+    row_pos = r;
+    col_pos = c;  
+  }
+
+
+
+
+
+}
